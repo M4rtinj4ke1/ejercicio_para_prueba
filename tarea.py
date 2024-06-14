e87619db1657a22c995328d924c1e7501160627e
MartinJaque <ma.jaqueq@duocuc.cl>
2024-06-14 13:19:58 -0400

    menu listo

---
+++ tarea.py
@@ -0,0 +1,21 @@
+import os, time
+while True:
+    os.system("cls")
+    print("""
+            MENU
+    1. Registrar trabajadores
+    2. Listar todo los trabajadores
+    3. Imprimir plantilla de sueldos
+    4. Salir del programa""")
+    opc = int(input('Ingrese una opcion: '))
+    os.system("cls")
+    if opc==1:
+        pass
+    elif opc==2:
+        pass
+    elif opc==3:
+        pass
+    else:
+        print ('gracias, adios')
+        break
+    time.sleep(2)
