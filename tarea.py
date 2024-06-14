c548b20f9a833c322d055004a286623dc56e3502
MartinJaque <ma.jaqueq@duocuc.cl>
2024-06-14 13:52:34 -0400

    opc 1 sin validar

--- tarea.py
+++ tarea.py
@@ -1,4 +1,6 @@
 import os, time
+trabajadores = []
+cargos = ('CEO','DESAROOLADOR','ANALISTA')
 while True:
     os.system("cls")
     print("""
@@ -10,7 +12,16 @@ while True:
     opc = int(input('Ingrese una opcion: '))
     os.system("cls")
     if opc==1:
-        pass
+        print ('Registro de trabajadores')
+        nombre_apellido = input('Ingrese nombre y apellido')
+        cargo = int(input('Ingrese cargo(1:CEO, 2:DESSARROLADOR, 3:ANALISTA): '))
+        sueldo_bruto = int(input('Ingrese sueldo bruto'))
+        salud = int(7/100 * sueldo_bruto)
+        afp = int(0.12 * sueldo_bruto)
+        sueldo_liquido = sueldo_bruto-(salud+afp)
+        trabajador = [nombre_apellido,cargos[cargo-1],sueldo_bruto,salud,afp,sueldo_liquido]
+        trabajadores.append(trabajador)
+        print('Trabajador registrado')
     elif opc==2:
         pass
     elif opc==3:
