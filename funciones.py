4195492d7962a9e1d794b60d9d5c68a4812f0847
MartinJaque <ma.jaqueq@duocuc.cl>
2024-06-14 14:55:00 -0400

    Opcion 2 agregada

--- funciones.py
+++ funciones.py
@@ -2,7 +2,7 @@ trabajadores = []
 cargos = ('CEO','DESAROOLADOR','ANALISTA')
 def registrar_trabajador():
     print ('Registro de trabajadores')
-    nombre_apellido = input('Ingrese nombre y apellido')
+    nombre_apellido =  input('Ingrese nombre y apellido')
     cargo = int(input('Ingrese cargo(1:CEO, 2:DESSARROLADOR, 3:ANALISTA): '))
     sueldo_bruto = int(input('Ingrese sueldo bruto'))
     salud = int(7/100 * sueldo_bruto)
@@ -12,8 +12,13 @@ def registrar_trabajador():
     trabajadores.append(trabajador)
     print('Trabajador registrado')
 
-def listar_trabajador():
- pass
+def listar_trabajadores():
+    if len(trabajadores)==0:
+      print('Lista vacia, registre un trbajador en la opcion 1')
+    else:
+      print('\tLista de trabajadores')
+      for t in trabajadores:
+         print(f'NOMBRE: {t[0]}\nCARGO: {t[1]}\nBRUTO: {t[2]}\nSALUD: {t[3]}\nAFP: {t[4]}\nLIQUIDO: {t[5]}')
 
 def exportar_archivo_txt():
  pass
